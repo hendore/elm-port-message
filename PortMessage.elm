72f78a453c351ba76562cebfd1990145bb0b002d
Thomas Henley <thenley28@googlemail.com>
2018-04-11 01:05:42 +0100

    Add PostMessage type and simple builder functions

---
+++ PortMessage.elm
@@ -0,0 +1,36 @@
+module PortMessage exposing (PortMessage, new, withPayload)
+
+{-| Add a short description here.
+
+
+# Builder
+
+@docs new, withPayload
+
+-}
+
+import Json.Encode
+
+
+type alias PortMessage =
+    { tag : Tag
+    , payload : Json.Encode.Value
+    }
+
+
+type alias Tag =
+    String
+
+
+{-| Create a new PortMessage with an empty payload.
+-}
+new : Tag -> PortMessage
+new tag =
+    PortMessage tag Json.Encode.null
+
+
+{-| Attach a payload to the PortMessage
+-}
+withPayload : Json.Encode.Value -> PortMessage -> PortMessage
+withPayload payload message =
+    { message | payload = payload }
